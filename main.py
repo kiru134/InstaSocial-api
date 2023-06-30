a97afb5825cec6cb6256d74d735c8286af8e7daa
kiran <kirankiru3434@gmail.com>
2023-06-30 13:35:20 +0530

    uploading images by giving relative path as posts functionality has been added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from db import models
 from db.database import engine
 from routers import users,posts
+from fastapi.staticfiles import StaticFiles
 
 
 
@@ -12,3 +13,5 @@ app.include_router(posts.router)
 
 
 models.Base.metadata.create_all(engine)
+
+app.mount('/images', StaticFiles(directory='images'), name='images')
--- routers/posts.py
+++ routers/posts.py
@@ -5,6 +5,9 @@ from routers.schemas import PostBase, PostDisplay
 from db.database import get_db
 from db import db_post
 from typing import List
+import random
+import string
+import shutil
 
 router = APIRouter(
   prefix='/post',
@@ -23,3 +26,16 @@ def create(request: PostBase, db: Session = Depends(get_db)):
 @router.get('/all', response_model=List[PostDisplay])
 def posts(db: Session = Depends(get_db)):
   return db_post.get_all(db)
+
+@router.post('/image')
+def upload_image(image: UploadFile = File(...)):
+  letters = string.ascii_letters
+  rand_str = ''.join(random.choice(letters) for i in range(6))
+  new = f'_{rand_str}.'
+  filename = new.join(image.filename.rsplit('.', 1))
+  path = f'images/{filename}'
+
+  with open(path, "w+b") as buffer:
+    shutil.copyfileobj(image.file, buffer)
+  
+  return {'filename': path}
